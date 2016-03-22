cd83f9513b568f302fb57eb05181c0a081667308
v.radev <ventsislav.radev@stanga.net>
2016-03-22 17:05:33 +0200

    Change of structure

---
+++ gulpfile.js/index.js
@@ -0,0 +1,9 @@
+/*
+  gulpfile.js
+  ===========
+*/
+
+var requireDir = require('require-dir');
+
+// Require all tasks in gulpfile.js/tasks, including subfolders
+requireDir('./tasks', { recurse: true });
