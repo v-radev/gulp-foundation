fcf633c300d76aead95934f7d43662647111497d
v.radev <ventsislav.radev@stanga.net>
2016-04-01 14:56:25 +0300

    CSS task finished

--- gulpfile.js/tasks/css.js
+++ gulpfile.js/tasks/css.js
@@ -9,19 +9,27 @@ var browserSync = require('browser-sync');
 var sass        = require('gulp-sass');
 var sourcemaps  = require('gulp-sourcemaps');
 var path        = require('path');
+var concat      = require('gulp-concat-css');
 var gulpUtil    = require('gulp-util');
 var env         = gulpUtil.env.env;
+var cssmin      = require('gulp-cssmin');
 
 var paths = {
     source: path.join( config.root.source, config.tasks.css.source, '/*.{' + config.tasks.css.extensions + '}' ),
     destination: path.join( config.root.destination, config.tasks.css.destination )
 };
 
+var cssSource = config.tasks.css.modules;
+
+cssSource.push( paths.source );
+
 var cssTask = function () {
-    return gulp.src( paths.source )
+    return gulp.src( cssSource )
         .pipe( env === 'production' ? gulpUtil.noop() : sourcemaps.init() )
         .pipe( sass().on('error', sass.logError) )
+        .pipe( concat('style.css') )
         .pipe( env === 'production' ? gulpUtil.noop() : sourcemaps.write('./') )
+        .pipe( env === 'production' ? cssmin() : gulpUtil.noop() )
         .pipe( gulp.dest( paths.destination ) )
         .pipe( env === 'production' ? gulpUtil.noop() : browserSync.stream() );
 };
