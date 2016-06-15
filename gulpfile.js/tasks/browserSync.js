748bcf4466ff89151a536a2b6909ff31f94fe74f
v.radev <ventsislav.radev@stanga.net>
2016-06-15 10:29:51 +0300

    Add flag to disable browser sync

--- gulpfile.js/tasks/browserSync.js
+++ gulpfile.js/tasks/browserSync.js
@@ -1,10 +1,17 @@
 var gulpUtil    = require('gulp-util');
 var env         = gulpUtil.env.env;
+var sync        = gulpUtil.env.sync;
 var gulp        = require('gulp');
 var browserSync = require('browser-sync');
 var config      = require('../config');
 
 if( env === 'production' ) {
+    module.exports = function(){ };
+    gulp.task('browserSync', function(){ });
+    return;
+}
+
+if( sync === 'false' ) {
     module.exports = function(){ };
     gulp.task('browserSync', function(){ });
     return;
@@ -17,5 +24,4 @@ var browserSyncTask = function() {
 };
 
 gulp.task('browserSync', browserSyncTask);
-
 module.exports = browserSyncTask;
